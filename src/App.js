8442e9035e358703b9d6a21e73022c5209399a2f
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-02 18:50:32 +0700

    add a text component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import OtherComponent from './components/OtherComponent';
 
 function App() {
     return (<div className="App">
         <MyComponent/>
+        <OtherComponent/>
         <MyComponent/>
+        <OtherComponent/>
     </div>);
 }
 
---
+++ src/components/OtherComponent.js
@@ -0,0 +1,10 @@
+function OtherComponent() {
+    return (<div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+            aspernatur assumenda beatae commodi cupiditate dicta exercitationem
+            fuga hic, impedit ipsa ipsam itaque laborum maiores maxime non quasi
+            sint veritatis vero.</p>
+    </div>);
+}
+
+export default OtherComponent;
